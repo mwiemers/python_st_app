9332ff86fd605f7b19a0d2d4a86231e1117d6527
Michael Wiemers <m.wiemers@lse.ac.uk>
2025-10-13 09:12:21 +0100

    updated blue highlight section at bottom of pages

--- pages/7_📙_Opening_Jupyter_Notebooks.py
+++ pages/7_📙_Opening_Jupyter_Notebooks.py
@@ -52,8 +52,7 @@ st.markdown(
 <br>
 
  <div class="highlight blue">
-    Come to the pre-sessional workshops if you struggle with any of the steps from this tutorial. You can find more information about the workshops in the 
-    <a href="https://dsl-python-presessional.streamlit.app/Pre-sessional_Workshops", target="_self">Python Pre-sessional workshops section</a>.
+    Our trainers can help you with fixing your installation issues at the start of the bootcamp.  
     </div>
     """, unsafe_allow_html=True
 )
